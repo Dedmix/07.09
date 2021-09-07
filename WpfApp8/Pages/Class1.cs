19b037825164df73b7e11e818d0db31a592760cc
Dedmix <84181853+Dedmix@users.noreply.github.com>
2021-09-07 11:51:44 +0500

    Add files via upload

---
+++ WpfApp8/Pages/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp8.Pages
+{
+    class Class1
+    {
+        public static Login PageLogin = new Login();
+        public static Registration PageReg = new Registration();
+        public static Start PageStart = new Start();
+
+    }
+}
---
+++ WpfApp8/Pages/Login.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp8.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if ((Логин.Text.Length==20D) && (Пароль.Text.Length ==25D))
+                {
+                MessageBox.Show("Не верно введён логин или пароль");
+                return;
+            }
+            NavigationService.Navigate(Class1.PageStart);
+
+           
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(Pages.Class1.PageReg);
+        }
+    }
+}
